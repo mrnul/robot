1238c8110c7e5b7944dae2080002d50fe65547ff
mrnul <kiriakidis.pm@gmail.com>
2025-11-22 12:47:35 +0200

    make functions inline

--- robotmc/main/include/utils.hpp
+++ robotmc/main/include/utils.hpp
@@ -2,12 +2,12 @@
 
 #include "freertos/FreeRTOS.h"
 
-void delay(int dur_ms)
+inline void delay(int dur_ms)
 {
     vTaskDelay(dur_ms / portTICK_PERIOD_MS);
 }
 
-uint16_t calculate_duration(float time_us, uint32_t resolution_hz)
+inline uint16_t calculate_duration(float time_us, uint32_t resolution_hz)
 {
     return uint16_t(time_us * resolution_hz / 1000000.0);
 }
